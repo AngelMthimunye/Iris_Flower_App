685a8c18f2b18e81f418a1436e0e84d948ee1032
Angel Mthimunye <62521227+AngelMthimunye@users.noreply.github.com>
2020-06-24 17:27:25 +0200

    Add files via upload

--- iris_app.py
+++ iris_app.py
@@ -7,18 +7,18 @@ clf_rf = pickle.load(open('Iris_Flower_Predictor.pkl', 'rb'))
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('contact.html')
 
 @app.route('/predict', methods=['POST'])
 def predict():
     '''
     For rendering results on HTML GUI
     '''
-    int_features = [float(x) for x in request.form.values()]
-    final_features = [np.array(int_features)]
+    float_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(float_features)]
     prediction = clf_rf.predict(final_features)
 
-    return render_template('index.html', prediction_text='The Iris Flower Species is {}:'.format(prediction))
+    return render_template('contact.html', prediction_text='The Iris Flower Species is {}'.format(prediction))
 
 if __name__ == "__main__":
     app.run(debug=True)
