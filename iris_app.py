46cf6221606d5005e59f810e05d2400033bf72bd
Angel Mthimunye <62521227+AngelMthimunye@users.noreply.github.com>
2020-06-24 08:42:30 +0200

    Add files via upload

---
+++ iris_app.py
@@ -0,0 +1,24 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+clf_rf = pickle.load(open('Iris_Flower_Predictor.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = clf_rf.predict(final_features)
+
+    return render_template('index.html', prediction_text='The Iris Flower Species is {}:'.format(prediction))
+
+if __name__ == "__main__":
+    app.run(debug=True)
