3ff743f06615242aecb77d42aab59160de211329
Angel Mthimunye <62521227+AngelMthimunye@users.noreply.github.com>
2020-06-27 09:43:11 +0200

    Delete request.py

--- request.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-url = "http://localhost:5000/predict_api"
-r = requests.post(url, json={"sepal_length":5.2, "sepal_width":3.8, "petal_length":1.5, "petal_width":0.2})
-
-print(r.json())
-
-
